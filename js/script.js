9549e9d061114840253f1424fa3d8ee5f0731fcd
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-01-10 11:45:11 +0100

    v 1.0

--- js/script.js
+++ js/script.js
@@ -3,13 +3,37 @@ function randomStuff(min, max){
   var pcNumber = Math.floor(Math.random() * (max + 1 - min) + min);
   return pcNumber;
 }
+function check (arr, usArr, us, t){
+  if (t === arr.length){
+    alert('Hai Vinto! Sei riuscito a fare ' + t + ' punti!')
+    return false
+  }
+  if (arr.indexOf(us) !== -1) {
+    alert('Hai Perso! Sei riuscito a fare ' + t + ' punti!')
+    return false
+  }
 
-var pcArray = [];
-var number
-
-for (var i = 0; i < 16; i++) {
-  number = randomStuff(1, 100);
-  pcArray.push(number);
 }
+var pcArray = [];
+var userArray = [];
+var number;
+var userN;
+var times;
+
+while (pcArray.length < 16) {
+  number = randomStuff(1, 100);
+  if (pcArray.indexOf(number) === -1){
+    pcArray.push(number);
+  }
+}
+console.log(pcArray)
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+times = 0;
+do {
+  userN = parseInt(prompt('inserisci un numero da 1 a 100!'));
+  userArray.push(userN);
+  times++;
+
+
+} while (check(pcArray, userArray, userN, times))
